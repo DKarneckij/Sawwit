413b0605c8385cf490b523f4e77d8c78b8fdaca3
Darius Karneckij <dkarneckij@gmail.com>
2024-03-03 00:31:25 -0500

    Working on NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,40 @@
-import { useState } from 'react'
-import './App.css'
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faHouse} from '@fortawesome/free-solid-svg-icons'
+import {faAngleDown} from '@fortawesome/free-solid-svg-icons'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+
+    <div>
+
+      <nav className="flex h-12 px-5">
+        
+        <div className='inline-flex  bg-blue-200'>
+          <a href="/" className='inline-flex w-fit bg-yellow-200'>
+            <img src="./assets/logo/logo-graphic.svg" alt="logo graphic" className="py-2 pr-2"/>
+            {/* <img src="./assets/logo/logo-name.svg" alt="logo name" className="h-auto py-3"/> */}
+          </a>
+          <div className=''>
+            <button className=''>
+              <FontAwesomeIcon icon={faHouse} />
+              Home
+              <FontAwesomeIcon icon={faAngleDown} />
+            </button>
+          </div>
+        </div>
+
+
+      </nav>
+
+
+      <main className='bg-gray-300 min-h-[calc(100vh-48px)]'>
+        test
+      </main>
+
+    </div>
+)}
 
 export default App
